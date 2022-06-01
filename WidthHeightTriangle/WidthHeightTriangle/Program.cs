5fd09ed2b239e158b7582dba1401a7cd62a8ff1c
VictorBwD <victor.bogdanowdornelles@gmail.com>
2022-06-01 00:09:37 -0300

    Area and Width triangle exercice

---
+++ WidthHeightTriangle/WidthHeightTriangle/Program.cs
@@ -0,0 +1,12 @@
+﻿WidthHeightTriangle.Triangle t;
+
+t = new WidthHeightTriangle.Triangle();
+
+Console.WriteLine("Triangle width: ");
+t.Width = float.Parse(Console.ReadLine());
+Console.WriteLine("Triangle height: ");
+t.Height = float.Parse(Console.ReadLine());
+
+Console.WriteLine("Triangle area: {0}", t.GetArea());
+Console.WriteLine("Triangle perimeter: {0}", t.Perimeter());
+Console.WriteLine("Triangle diagonal: {0}", t.Diagonal());
---
+++ WidthHeightTriangle/WidthHeightTriangle/Triangle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WidthHeightTriangle
+{
+    internal class Triangle
+    {
+        public float Width { get; set; }
+        public float Height { get; set; }
+
+        public float GetArea()
+        {
+            return Width * Height;
+        }
+
+        public double Perimeter()
+        {
+            return Width + Height + Math.Sqrt(Width * Width + Height * Height);
+        }
+
+        public float Diagonal()
+        {
+            return (float)Math.Sqrt(Width * Width + Height * Height);
+        }
+    }
+}
