b38064d028a3482fc8efac844d12a5f60ba42abe
VictorBwD <victor.bogdanowdornelles@gmail.com>
2022-06-01 01:05:54 -0300

    Commit dolar converter to real

---
+++ RealToDolarConvert/RealToDolarConvert/CoinConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DolarToRealConvert
+{
+    class CoinConverter
+    {
+        public static float IOF = 6.0f;
+
+        public static float DolarToReal(float quantity, float price)
+        {
+            float total = quantity * price;
+            return total + (total * IOF / 100);
+        }
+    }
+}
---
+++ RealToDolarConvert/RealToDolarConvert/Program.cs
@@ -0,0 +1,11 @@
+﻿using DolarToRealConvert;
+
+Console.WriteLine("What is the dollar price?");
+float price = float.Parse(Console.ReadLine());
+
+Console.WriteLine("How many dollars?");
+float quantity = float.Parse(Console.ReadLine());
+
+float result = CoinConverter.DolarToReal(quantity, price);
+
+Console.WriteLine("The total value is: " + result);
