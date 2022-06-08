5caeb8b68fe4deebc990ef5d8aa4d82093c587a7
VictorBwD <victor.bogdanowdornelles@gmail.com>
2022-06-07 23:07:58 -0300

    Bank Problem added

---
+++ BankProblem/Bank_Problem/ContaBancaria.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bank_Problem
+{
+    class ContaBancaria
+    {
+        public int NumeroDaConta { get; private set; }
+        public string Titular { get; set; }
+        public double Saldo { get; private set; }
+
+
+        public ContaBancaria()
+        {
+
+        }
+
+        public ContaBancaria(int numerodaconta, string titular)
+        {
+            NumeroDaConta = numerodaconta;
+            Titular = titular;
+            Saldo = 0;
+        }
+
+        public ContaBancaria(int numerodaconta, string titular, double depositoIncial) : this(numerodaconta, titular)
+        {
+            Deposito(depositoIncial);
+        }
+
+        public void Deposito(double quantia)
+        {
+            Saldo = Saldo + quantia;
+        }
+
+        public void Saque(double quantia)
+        {
+            Saldo = Saldo - quantia;
+            Saldo -= 5.0;
+        }
+
+
+
+        public override string ToString()
+        {
+            return "Conta " + NumeroDaConta + ", Titular: " + Titular + ", Saldo: $ " + Saldo.ToString("F2");
+        }
+    }
+}
---
+++ BankProblem/Bank_Problem/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Globalization;
+
+namespace Bank_Problem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bem vindo ao banco VICTOR! Por favor, digite as informações abaixo...");
+            //declarar a classe aqui em cima para usa-la fora do if
+            ContaBancaria conta;
+
+
+            Console.Write("Digite o numero da conta: ");
+            int numero = int.Parse(Console.ReadLine());//criar a variavel para receber o numero da conta
+            Console.Write("Digite o nome do titular da conta: ");
+            string titular = Console.ReadLine();//criar a variavel para receber o titular
+            Console.Write("Depósito inicial? (s/n)");
+            char resp = char.Parse(Console.ReadLine()); //criar a variavel para receber a resposta
+            if (resp == 's' || resp == 'S') //se a resposta for igual a sim
+            {
+                Console.Write("Entre com o valor do depósito: ");
+                double depositoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                conta = new ContaBancaria(numero, titular, depositoInicial); //cria o constructor com 3 argumentos para receber o valor do deposito
+
+            }
+            else
+            {
+                conta = new ContaBancaria(numero, titular);// caso for não então cria o constructor de 2 argumentos para ter o saldo 0
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta: ");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.Write("Entre com o valor de depósito: ");
+            double dep = double.Parse(Console.ReadLine()); //cria a variavel para receber o valor do deposito
+            conta.Deposito(dep); // passa como parametro a variavel para a função na classe
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(conta);// printa a classe
+
+            Console.WriteLine();
+            Console.Write("Entre com o valor do saque: ");
+            double saque = double.Parse(Console.ReadLine()); //cria a variavel para receber o valor do deposito
+            conta.Deposito(saque); // passa como parametro a variavel para a função na classe
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(conta);// printa a classe
+
+
+
+
+        }
+    }
+}
