37aeb79f7bb1d0f53d605e1a1650592cc50fa3cf
VictorBwD <victor.bogdanowdornelles@gmail.com>
2022-06-12 21:38:17 -0300

    Exercice list added

---
+++ ExerciceList/ExerciceList/Employee.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciceList
+{
+    internal class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public float Salary { get; set; }
+
+        public Employee(int id, string name, float salary)
+        {
+            Id = id;
+            Name = name;
+            Salary = salary;
+        }
+        
+        public void IncreaseSalary(float percentage)
+        {
+            Salary += Salary * percentage / 100;
+        }
+
+        public override string ToString()
+        {
+            return Id
+                + ", "
+                + Name
+                + ", "
+                + Salary.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ ExerciceList/ExerciceList/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.WriteLine("How many employees do you want to add?");
+int numberOfEmployees = int.Parse(Console.ReadLine());
+
+List<ExerciceList.Employee> employees = new List<ExerciceList.Employee>();
+
+for( int i = 1; i <= numberOfEmployees; i++)
+{
+    Console.WriteLine("Employee #" + i + ":");
+    Console.Write("Id: ");
+    int id = int.Parse(Console.ReadLine());
+    Console.Write("Name: ");
+    string name = Console.ReadLine();
+    Console.Write("Salary: ");
+    float salary = float.Parse(Console.ReadLine());
+    employees.Add(new ExerciceList.Employee(id, name, salary));
+}
+
+Console.WriteLine("Enter the employee id that will have salary increase: ");
+int searchId = int.Parse(Console.ReadLine());
+ExerciceList.Employee emp = employees.Find(match: x => x.Id == searchId);
+if(emp != null)
+{
+    Console.WriteLine("Enter the percentage: ");
+    float percentage = float.Parse(Console.ReadLine());
+    emp.IncreaseSalary(percentage);
+}
+else
+{
+    Console.WriteLine("Employee not found!");
+}
+
+Console.WriteLine();
+Console.WriteLine("Updated list of employees: ");
+foreach (ExerciceList.Employee obj in employees)
+{
+    Console.WriteLine(obj);
+}
+
