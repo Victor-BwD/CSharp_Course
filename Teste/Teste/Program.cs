511e968a5299fb6f936a5a693b245e906831682c
Victor Bogdanow Dornelles <victor.bogdanowdornelles@gmail.com>
2022-07-02 22:25:17 -0300

    Single resposibility principle added

---
+++ Teste/Teste/Program.cs
@@ -0,0 +1,22 @@
+﻿List<int> list = new List<int> { 1, 2, 3, 4, 5};
+List<int> par = new List<int>();
+List<int> impar = new List<int>();
+
+foreach (int obj in list)
+{
+    if(obj % 2 == 0)
+    {
+        par.Add(obj);
+    }
+    else
+    {
+        impar.Add(obj);
+    }
+}
+
+    Console.WriteLine("{0}", string.Join(", ", list));
+Console.WriteLine("{0}", string.Join(", ", par));
+Console.WriteLine("{0}", string.Join(", ", impar));
+
+
+
